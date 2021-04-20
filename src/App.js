ab00c0feab94e1b7741fa2f8f001cd72deb2f0e5
Jean Esteves <jean.esteves2001@gmail.com>
2021-04-19 21:14:46 -0300

    estudo de conceitos componente propriedade e estado

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
+import Button from './Button';
+
 function App() {
   return (
-    <h1>Hello World!!</h1>
+    <>
+    <Button>Button 1</Button>
+    <Button>Button 2</Button>
+    <Button>Button 3</Button>
+    <Button>Button 4</Button>
+    <Button>Button 5</Button>
+    </>
   );
 }
 
